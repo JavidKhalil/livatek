f898ab4dbd20c5a31834047573f0626d1c56f2a8
javid <cavid_xalilov@mail.ru>
2022-11-10 14:13:59 +0400

    Initial commit

---
+++ src/main/java/com/example/livatek/LivatekApplication.java
@@ -0,0 +1,13 @@
+package com.example.livatek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivatekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivatekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/livatek/LivatekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livatek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivatekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
